33247d0e6049679ae9650e1e464a188ae2ceaa85
Nishant4521 <nishant.agrawal2025@glbajajgroup.org>
2026-06-10 13:22:31 +0530

    Day-3 assignment

---
+++ Day-3/to_find_GCD_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{int num1,num2,i,gcd = 1;
+printf("Enter the value of two numbers");
+scanf("%d %d", &num1, &num2);
+for (i=1;i<=num1 && i<=num2;i++) 
+{if (num1%i==0 && num2%i==0) 
+{gcd = i; 
+}
+}
+ printf("GCD is: %d", gcd);
+ return 0;
+}

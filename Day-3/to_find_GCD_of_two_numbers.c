6b2aed05d80a89f0d207321a5ccb721c42cb6a74
Nishant4521 <nishant.agrawal2025@glbajajgroup.org>
2026-06-11 11:15:23 +0530

    cleaning up Day-3

--- Day-3/to_find_GCD_of_two_numbers.c
+++ Day-3/to_find_GCD_of_two_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<conio.h>
 int main()
-{int num1,num2,i,gcd = 1;
+{int num1,num2,i,gcd=1;
 printf("Enter the value of two numbers");
 scanf("%d %d", &num1, &num2);
 for (i=1;i<=num1 && i<=num2;i++) 
